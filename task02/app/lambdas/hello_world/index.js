8ee41f70d9987df8e9b320d2623e94764fdbc33a
Abhay Arora <abhayarora@my.unt.edu>
2024-08-23 12:58:58 -0400

    updated response message

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -10,14 +10,18 @@ exports.handler = async (event) => {
         console.log("inside success ")
         const successResponse = {
             statusCode: 200,
-            message: 'Hello from Lambda',
+            body: JSON.stringify({
+                message: 'Hello from Lambda',
+            }),
         };
         return successResponse;
     } else {
         console.log("inside failure ")
         const failedResponse = {
             statusCode: 400,
-            message: `Bad request syntax or unsupported method. Request path: ${httpPath}. HTTP method: ${httpMethod}`
+            body: JSON.stringify({
+                message: `Bad request syntax or unsupported method. Request path: ${httpPath}. HTTP method: ${httpMethod}`,
+            }),
         };
         return failedResponse;
     }

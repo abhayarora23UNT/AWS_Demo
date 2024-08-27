03f3f511352fef49904d1cb0c61d7f64e3090dcd
Abhay Arora <abhayarora@my.unt.edu>
2024-08-26 21:44:33 -0400

    task 03 api gateway

---
+++ task03/app/lambdas/hello_world/index.js
@@ -0,0 +1,18 @@
+exports.handler = async (event) => {
+    console.log("http event ",event)
+    const httpMethod = event.requestContext.http.method
+    const httpPath = event.requestContext.http.path
+    const buildResponse = (statusCodeVal, messageVal) => ({
+        statusCode: statusCodeVal,
+        body: JSON.stringify({ statusCode: statusCodeVal, message: messageVal }),
+    });
+
+    if (httpMethod === 'GET' && httpPath === '/hello') {
+        return buildResponse(200, 'Hello from Lambda');
+    } else {
+        return buildResponse(
+            400,
+            `Bad request syntax or unsupported method. Request path: ${httpPath}. HTTP method: ${httpMethod}`
+        );
+    }
+};

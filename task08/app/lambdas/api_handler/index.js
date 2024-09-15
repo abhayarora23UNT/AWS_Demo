566a35d8d01c59f026c826e61ff67b1c56ec4477
Abhay Arora <abhayarora@my.unt.edu>
2024-09-15 12:39:12 -0400

    weather client lambda

--- task08/app/lambdas/api_handler/index.js
+++ task08/app/lambdas/api_handler/index.js
@@ -1,8 +1,19 @@
+const { getWeather } = require('/opt/nodejs/weatherClient');  // '/opt/nodejs' is the path where Lambda layers are mounted
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+    console.log("api handler lambda event is ", event)
+    const latitude = event.latitude
+    const longitude = event.longitude
+
+    try {
+        const weatherData = await getWeather(latitude, longitude);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(weatherData),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error.message }),
+        };
+    }
 };

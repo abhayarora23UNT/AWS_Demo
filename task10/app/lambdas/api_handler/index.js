464b4e7d30a6b7399fb90d3ab3c4f60b2eaf039c
Abhay Arora <abhayarora@my.unt.edu>
2024-09-15 18:06:10 -0400

    lambda changes for auth

--- task10/app/lambdas/api_handler/index.js
+++ task10/app/lambdas/api_handler/index.js
@@ -5,30 +5,71 @@ const userPoolId = process.env.CUPId;
 const clientId = process.env.CUPClientId;
 
 const cognitoIdentityServiceProvider = new CognitoIdentityServiceProvider();
-exports.handler = async (event) => {
-    console.log("+++lambda event is ",event);
+
+export const buildResponse = (statusCodeVal, messageVal) => ({
+    statusCode: statusCodeVal,
+    body: JSON.stringify({ statusCode: statusCodeVal, message: messageVal }),
+});
+async function performCognitoSignUp(event) {
     const body = JSON.parse(event.body);
     const params = {
         AuthFlow: 'ADMIN_NO_SRP_AUTH',
         ClientId: clientId,
+        UserPoolId, userPoolId,
         AuthParameters: {
             USERNAME: body.email,
-            PASSWORD: body.password   
-        }
+            PASSWORD: body.password
+        },
+        MessageAction: 'SUPPRESS'
     };
-
     try {
-        const data = await cognitoIdentityServiceProvider.initiateAuth(params).promise();
-        const idToken = data.AuthenticationResult.IdToken;
-        console.log("+++idToken  ",idToken);
-        //return idToken;
+        const createUserData = await cognitoIdentityServiceProvider.adminCreateUser(params).promise();
+        console.log('User created successfully:', createUserData);
+        return buildResponse(200, 'User created successfully');
     } catch (error) {
         console.error(error);
-        throw error;
+        return buildResponse(
+            400,
+            `Bad request syntax or unsupported method. Request path: ${httpPath}. HTTP method: ${httpMethod}`
+        );
     }
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+}
+async function performCognitoSignIn(event) {
+    const body = JSON.parse(event.body);
+    const params = {
+        AuthFlow: 'USER_PASSWORD_AUTH',
+        ClientId: clientId,
+        UserPoolId, userPoolId,
+        AuthParameters: {
+            USERNAME: body.email,
+            PASSWORD: body.password
+        },
+        MessageAction: 'SUPPRESS'
     };
-    return response;
+    try {
+        const data = await cognitoIdentityServiceProvider.initiateAuth(params).promise();
+        console.log('Sign-in successful', data);
+        return buildResponse(200, 'Sign-in successful');
+    } catch (error) {
+        console.error(error);
+        return buildResponse(
+            400,
+            `Bad request syntax or unsupported method. Request path: ${httpPath}. HTTP method: ${httpMethod}`
+        );
+    }
+}
+exports.handler = async (event) => {
+    console.log("+++lambda event is ", event);
+    const httpMethod = event.requestContext.http.method
+    const httpPath = event.requestContext.http.path
+    if (httpMethod === 'POST' && httpPath === '/signup') {
+        performCognitoSignUp(event)
+    } else if (httpMethod === 'POST' && httpPath === '/signin') {
+        performCognitoSignIn(event)
+    } else {
+        return buildResponse(
+            500,
+            `Internal Server Error. Request path: ${httpPath}. HTTP method: ${httpMethod}`
+        );
+    }
 };

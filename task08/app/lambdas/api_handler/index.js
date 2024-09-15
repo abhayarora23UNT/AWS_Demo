7ec0dbfb4068532b1e53dadc04be259ece8eab5d
Abhay Arora <abhayarora@my.unt.edu>
2024-09-15 13:06:19 -0400

    update lat long

--- task08/app/lambdas/api_handler/index.js
+++ task08/app/lambdas/api_handler/index.js
@@ -1,11 +1,12 @@
 const { getWeather } = require('/opt/nodejs/weather-client');  // '/opt/nodejs' is the path where Lambda layers are mounted
 exports.handler = async (event) => {
     console.log("api handler lambda event is ", event)
-    const latitude = event.latitude
-    const longitude = event.longitude
+    const latitude = event.latitude | 50.4375
+    const longitude = event.longitude | 30.5
 
     try {
         const weatherData = await getWeather(latitude, longitude);
+        console.log("weatherData is ", weatherData)
         return {
             statusCode: 200,
             body: JSON.stringify(weatherData),
